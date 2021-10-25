6662832ff4c7688786a28a588352574de8e942d6
lovelyring7 <90598775+lovelyring7@users.noreply.github.com>
2021-10-25 16:58:16 -0700

    Add files via upload

---
+++ itemlist.php
@@ -0,0 +1,85 @@
+<?php
+
+
+	$details=array("name", "price", "photo");
+
+
+	// item1
+	$item1 = [
+		"name" => "C line skirt",
+		"price" => "$65", 
+		"photo" => "image/item1.jpg"
+	];
+
+	$item2 = [
+		"name" => "A line skirt2",
+		"price" => "$97", 
+		"photo" => "image/item2.jpg"
+	];
+
+	$item3 = [
+		"name" => "A line skirt3",
+		"price" => "$77", 
+		"photo" => "image/item3.jpg"
+	];
+
+	$item4 = [
+		"name" => "A line skirt4",
+		"price" => "$19", 
+		"photo" => "image/item4.jpg"
+	];
+
+		$item5 = [
+		"name" => "A line skirt5",
+		"price" => "$38", 
+		"photo" => "image/item5.jpg"
+	];
+
+	$item6 = [
+		"name" => "A line skirt6",
+		"price" => "$34", 
+		"photo" => "image/item6.jpg"
+	];
+
+	$item7 = [
+		"name" => "A line skirt7",
+		"price" => "$75", 
+		"photo" => "image/item7.jpg"
+	];
+
+	$item8 = [
+		"name" => "A line skirt8",
+		"price" => "$23", 
+		"photo" => "image/item8.jpg"
+	];
+
+
+	$item9 = [
+		"name" => "A line skirt9",
+		"price" => "$65", 
+		"photo" => "image/item9.jpg"
+	];
+
+	$item10 = [
+		"name" => "A line skirt10",
+		"price" => "$65", 
+		"photo" => "image/item10.jpg"
+	];
+
+	$item11 = [
+		"name" => "A line skirt11",
+		"price" => "$35", 
+		"photo" => "image/item11.jpg"
+	];
+
+	$item12 = [
+		"name" => "A line skirt12",
+		"price" => "$79", 
+		"photo" => "image/item12.jpg"
+	];
+
+
+
+
+
+	?>
